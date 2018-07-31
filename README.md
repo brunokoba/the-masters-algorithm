979382a6b51ead10f89f3daca8861c63e2070890
brunoaks <brunoaks@gmail.com>
2018-07-31 19:56:18 +0900

    fully functional version

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # the_masters_algorithm
-A web scraper built with Python and Beautiful Soup for collecting data of the best Masters in Engineering in the US.
+A web scraper built with Python and Beautiful Soup for collecting data of the Top 75 Masters in Engineering in the US, according the USNews rankings. 
+
+The features collected are as follows:
+* Rank
+* School Name
+* Location
+* Application deadline for international applicants
+* Application fee for international applicants
+* Size of full-time faculty
+* Size of full-time student body
+* Full-time tuition
+* Link for each school's engineering department
+
+Feel free to take a look at the code and tweak it to fit your own needs. Beautiful Soup provides a very neat and intuitive way of parsing HTML files. 
+
+The scraper was built using Python 3, so make sure you have that installed on your machine before running the script.
+
+## Instructions
+1) Press the green **Clone or download** button in this repository.
+2) Decide if you want to Clone the repo or download it as .zip (I'd suggest downloading as .zip if you're not familiar with Github).
+3) Open the project folder and install project requirements by running `pip install -r 'requirements.txt'` in your terminal
+4) Run `python script.py` in your terminal
+5) Voilà, the file `database.csv` should be up-to-date. You can open it with your favourite spreadsheet software as well. 
--- script.py
+++ script.py
@@ -7,18 +7,24 @@ import pandas as pd
 
 # Initializing writer 
 csv_file = open('database.csv', 'w', buffering=1, encoding='utf8')
+csv_file.write('\ufeff')
 writer = csv.writer(csv_file, lineterminator = '\n')
 
 # List of URLs to be scraped
-urls = ['https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings',\
-        'https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings/page+2']
+urls = ['https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings',
+        'https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings/page+2',
+        'https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings/page+3']
 
-# Initializing features to be collected (each list will be a column in the final .csv file)
+# Initializing features to be collected (each list will be a column in the final .csv file, except "univ_pages")
 names = []
 locations = []
-tuitions = []
-enrollments = []
 univ_pages = []
+inter_deadlines = []
+inter_fees = []
+ft_teachers = []
+ft_enrollments = []
+ft_tuitions = []
+admissions_links = []
 records = []
 
 # Setting agent for scraping authentication
@@ -28,36 +34,89 @@ headers = {'User-Agent':'Mozilla/5.0'}
 for url in urls:
     
     # Requesting page and saving them as soup objects
-    r = requests.get(url, headers=headers)
-    data = soup(r.text, "html5lib")
+    req = requests.get(url, headers=headers)
+    data = soup(req.text, "html5lib")
     
-    # Appending each found value to the features lists
+    # Appending each found value to the first three features
     for name in data.findAll('a', attrs={'class': 'school-name'}):
         names.append(name.string)
     for location in data.findAll('p', attrs={'class': 'location'}):
         locations.append(location.string)
     for univ_page in data.findAll('a', attrs={'class': 'school-name', 'href': True}):
         univ_pages.append(univ_page['href'])
-    for tuition in data.findAll('td', attrs={'span': 'column-odd table-column-odd search_tuition'}):
-        tuitions.append(tuition.string)
-        print(tuition)
-    for enrollment in data.findAll('td', attrs={'class': 'column-even table-column-even  total_enrolled  '}):
-        enrollments.append(enrollment.string)
-        print(enrollments)
         
+# Extracting information from new URLs, which specific to each university
+for univ_page in univ_pages:    
+    new_url = 'https://www.usnews.com' + univ_page
+    req2 = requests.get(new_url, headers=headers)
+    data2 = soup(req2.text, "html5lib")
+    
+    # Collecting international deadline data
+    inter_deadline = data2.find('td', attrs={'class':"column-last", 'data-test-id':"v_international_deadline"})
+    try:    
+        inter_deadlines.append(inter_deadline.text.strip())
+    except:
+        inter_deadlines.append(None)
+    
+    # Collecting international application fee data
+    inter_fee = data2.find('td', attrs={'class':"column-last", 'data-test-id':"intl_application_fee"})
+    try:
+        inter_fees.append(inter_fee.text.strip())
+    except:
+        inter_fees.append(None)
+    
+    # Collecting full-time faculty data
+    ft_teacher = data2.find('td', attrs={'class':"column-last", 'data-test-id':"ft_teachers"})
+    try:
+        ft_teachers.append(ft_teacher.text.strip())
+    except:
+        ft_teachers.append(None)
+        
+    # Collecting full-time enrollment data
+    ft_enrollment = data2.find('td', attrs={'class':"column-last", 'data-test-id':"v_ft_enrolled_dir_pg"})
+    try:
+        ft_enrollments.append(ft_enrollment.text.strip())
+    except:
+        ft_enrollments.append(None)
+        
+    # Collecting full-time tuition data
+    ft_tuition = data2.find('tr', attrs={'class':"extra-row v_mas_tuition_ft-extra-row"})
+    try:
+        ft_tuitions.append(ft_tuition.text.strip())
+    except:
+        ft_tuitions.append(None)
+        
+    # Collecting admissions links
+    admissions_link = data2.find('a', attrs={'class':"t-strong", 'id': 'moreinfo_link', 'href':True})['href']
+    try:
+        admissions_links.append(admissions_link)
+    except:
+        admissions_links.append(None)    
+
 # Add titles
-titles = ['Rank', 'School', 'Location', 'USNews URL']
+titles = ['Rank', 'School', 'Location', 'Application Deadline', 
+          'Application Fee', 'Full-time Faculty', 'Full-time enrollment', 
+          'Full-time tuition', 'Grad admissions link']
+
+# Write table titles
 writer.writerow(titles)
 
-# Add data to the .csv file row by row
+# Add data to be written in .csv file, row by row
 for i in range(len(names)):
-    records.append(i+1)
-    records.append(names[i])
-    records.append(locations[i])
-    records.append(univ_pages[i])
+    records.extend([i+1,
+                    names[i],
+                    locations[i],
+                    inter_deadlines[i],
+                    inter_fees[i],
+                    ft_teachers[i],
+                    ft_enrollments[i],
+                    ft_tuitions[i],
+                    admissions_links[i]])
+    
+    # Writing data into .csv file and reinitilizing storage variable
     writer.writerow(records)
     records = []
     
 # Reading as a pandas DataFrame    
 df = pd.read_csv('database.csv')
-print(df)
+df.head()
